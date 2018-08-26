66723ae3c1d5f84dc2655a931b7c475498b03910
Vladimir Danilov <diasonti@gmail.com>
2018-08-26 16:50:35 +0600

    update

--- src/main/java/com/sonti/chatnoir/ChatNoirApplication.java
+++ src/main/java/com/sonti/chatnoir/ChatNoirApplication.java
@@ -1,16 +1,13 @@
 package com.sonti.chatnoir;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class ChatNoirApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChatNoirApplication.class, args);
-        log.info("Logger initialised");
     }
 
 }
