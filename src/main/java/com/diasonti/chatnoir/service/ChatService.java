8ca5efa965f9b1f4733f4cd0a189429abede7920
Vladimir Danilov <diasonti@gmail.com>
2018-08-30 02:22:04 +0600

    Add sender null check

--- src/main/java/com/diasonti/chatnoir/service/ChatService.java
+++ src/main/java/com/diasonti/chatnoir/service/ChatService.java
@@ -5,6 +5,7 @@ import com.diasonti.chatnoir.entity.ChatMessage;
 import com.diasonti.chatnoir.entity.ChatUserAccount;
 import com.diasonti.chatnoir.repository.ChatMessageRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor(onConstructor_={@Autowired})
 public class ChatService {
 
@@ -26,16 +28,20 @@ public class ChatService {
     }
 
     public void postMessage(ChatUser sender, String text) {
+        if(sender == null) {
+            log.error("Method postMessage error: Sender should not be null");
+            return;
+        }
         ChatUserAccount senderAccount = sender.getUserAccount();
         ChatMessage message = new ChatMessage();
         message.setSentAt(LocalDateTime.now());
         message.setSenderId(senderAccount.getId());
         message.setText(text);
         messageRepository.save(message);
-        trim();
+        trimMessages();
     }
 
-    private void trim() {
+    private void trimMessages() {
         long count = messageRepository.count();
         if(count > messageLimit)
             deleteOldestMessage();
